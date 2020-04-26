c66a5cc812fbf7d532882beb29269a3b7d93282a
pablo <pablolescano87@hotmail.com>
2020-04-26 12:13:32 -0300

    Subo solución 2do ejercicio

---
+++ PracticaParcial1_Ejercicio2/PracticaParcial1_Ejercicio2/Producto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PracticaParcial1_Ejercicio2
+{
+    class Producto : PedidoComponent
+    {
+        public Producto(string nombre, decimal precio, int cantidad) : base(nombre, precio, cantidad)
+        {
+            this.Nombre = nombre;
+            this.Precio = precio;
+            this.Cantidad = cantidad;
+        }
+    }
+}
